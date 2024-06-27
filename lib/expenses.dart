0fcbdb124420bc6097ac16c5456c20b303e6ea7d
rambutan <pride.kraievskyi@gmail.com>
2024-06-27 10:14:49 +0300

    add List view

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:expense_tracker_app/models/expense.dart';
+import 'package:expense_tracker_app/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -14,26 +15,26 @@ class Expenses extends StatefulWidget {
 class _ExpensesState extends State<Expenses> {
   final List<Expense> _registerExpenses = [
     Expense(
-      title: 'Flutter course',
-      amount: 19.99,
-      date: DateTime.now(),
-      category: Category.work
-    ),
+        title: 'Flutter course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
     Expense(
-      title: 'Cinema',
-      amount: 15.44,
-      date: DateTime.now(),
-      category: Category.leisure
-    ),
+        title: 'Cinema',
+        amount: 15.44,
+        date: DateTime.now(),
+        category: Category.leisure),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          Text('The chart'),
-          Text('Expenses list ...'),
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registerExpenses),
+          )
         ],
       ),
     );
